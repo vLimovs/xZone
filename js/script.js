fa220cceef886ddea09e23576b8408ed286a410e
vLimovs <limonytchannel@gmail.com>
2024-11-25 23:55:41 +0500

    New effect

--- js/script.js
+++ js/script.js
@@ -1,14 +1,14 @@
 class Text {
-    constructor(obj){
+    constructor(obj) {
         this.text = document.querySelector(obj.text);
         this.fullText = this.text.innerHTML
         this.text.innerHTML = ''
         this.str()
     }
-    str(x = 0){
+    str(x = 0) {
         this.text.innerHTML += this.fullText[x]
         x++
-        if(x < this.fullText.length){   
+        if (x < this.fullText.length) {
             setTimeout(() => {
                 this.str(x)
             }, 100);
@@ -18,4 +18,84 @@ class Text {
 
 const text = new Text({
     text: '.topic'
+})
+
+// class Scroll {
+//     constructor(obj) {
+//         this.section = document.querySelector(obj.section);
+
+//         window.addEventListener('scroll', () => {
+//             this.fadeRightAnim(this.section, 0.5)
+//         })
+//     }
+//     fadeRightAnim(section, coordinate) {
+
+//         const fadeRights = section.querySelectorAll('.fade-right');
+//         fadeRights.forEach(box => {
+//             const speed = box.dataset.speed; 
+//             box.style.transition = speed + 'ms';
+        
+//             const threshold = section.offsetTop - section.offsetHeight * coordinate;
+        
+//             console.log('window.scrollY:', window.scrollY);
+//             console.log('threshold:', threshold);
+        
+//             // Условие
+//             if (window.scrollY >= threshold) {
+//                 box.classList.add('active');
+//             } else {
+//                 box.classList.remove('active');
+//             }
+//         });
+//     }
+// }
+class Scroll {
+    constructor(obj) {
+        this.section = document.querySelector(obj.section);
+
+        window.addEventListener('scroll', () => {
+            this.fadeRightAnim(this.section, 0.9)
+            this.fadeLeftAnim(this.section, 0.4)
+        })
+    }
+    fadeRightAnim(section, coordinate) {
+        const fadeRights = section.querySelectorAll('.fade-right');
+        fadeRights.forEach(box => {
+            const speed = box.dataset.speed; 
+            box.style.transition = speed + 'ms';
+        
+            const threshold = section.offsetTop - section.offsetHeight * coordinate;
+        
+            console.log('window.scrollY:', window.scrollY);
+            console.log('threshold:', threshold);
+        
+            if (window.scrollY >= threshold) {
+                box.classList.add('active');
+            } else {
+                box.classList.remove('active');
+            }
+        });
+    }
+    fadeLeftAnim(section, coordinate) {
+        const fadeLefts = section.querySelectorAll('.fade-left');
+        fadeLefts.forEach(box => {
+            const speed = box.dataset.speed; 
+            box.style.transition = speed + 'ms';
+        
+            const threshold = section.offsetTop - section.offsetHeight * coordinate;
+        
+            console.log('window.scrollY:', window.scrollY);
+            console.log('threshold:', threshold);
+        
+            if (window.scrollY >= threshold) {
+                box.classList.add('active');
+            } else {
+                box.classList.remove('active');
+            }
+        });
+    }
+}
+
+const section = new Scroll({
+    section: '.service__cards'
 })
